c6098e123d77ef70a209df7c8d567366bd676f30
Christian Danielsen <chribsen@yahoo.dk>
2016-01-08 16:57:46 +0000

    Config file for directory dependencies

---
+++ config.py
@@ -0,0 +1,7 @@
+paths = {
+'mallet_bin': '/large_volume/usr/bin/mallet/bin/',
+'mallet': '/large_volume/usr/bin/mallet/bin/mallet',
+'classifier': '/large_volume/usr/share/nginx/html/5feelings_angry_animated_empowered_fearful_joy.classifier.trial0',
+'input_dir':'temp/'
+
+}
