75ce9c2fbe22cc8a0728e7fc096f48929798a14c
Michele Colombo <mikexine@gmail.com>
2017-02-11 15:33:07 +0100

    clean config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ warnings.filterwarnings("ignore")
 
 six_way = 'prediction_models/six_way.pkl'
 six_way_labels = ('sw_anger', 'sw_excitement', 'sw_empowered',
-                  'sw_fearful', 'sw_joyful', 'sw_sadness')
+                  'sw_fearful', 'sw_joy', 'sw_sadness')
 
 pos_neg = 'prediction_models/pos_neg.pkl'
 pos_neg_labels = ('pn_negative', 'pn_positive')
@@ -50,17 +50,3 @@ models = [
 
 
 input_dir = 'temp/'
-
-
-# output_columns = {
-# #'5_way_classifier': ('angry', 'animated', 'empowered', 'fearful', 'joy'),
-# '6_way_classifier': ('ANGER', 'EXCITEMENT', 'EMPOWERED', 'FEARFUL', 'JOY', 'SADNESS'),
-# 'aroused_calm_classifier': ('aroused_excited_angry_pissed', 'calm_tired_relaxed_sleepy'),
-# 'pos_neg_classifier': ('neg_sad_disgusted_disappointed', 'pos_happy_great_wonderful'),
-# '27_way_classifier': ('accomplished', 'amused', 'angry', 'annoyed', 'awesome', 'bad', 'confident', 'confused', 'depressed', 'determined', 'disappointed', 'disgusted', 'down', 'excited', 'fantastic', 'great', 'happy', 'heartbroken', 'hopeful', 'pissed', 'proud', 'pumped', 'sad', 'scared', 'super', 'wonderful', 'worried')
-# }
-
-# column_prefix = {
-# '6_way_classifier': '6_way_',
-# '27_way_classifier': '27_way_'
-# }
